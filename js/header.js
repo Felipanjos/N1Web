05bbdfbe90dd4c854d025c1aaaeca3c84aa6cdac
Felipanjos <fanjos.santos@outlook.com>
2021-09-21 00:01:35 -0300

    Novo botão de produtos, arquivos JS isolado, footer preso

---
+++ js/header.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#header").load("header.html"); 
+});
+
+$(function(){
+    $("#headerProdutos").load("headerProdutos.html");
+});
--- script.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $("#adicionaHeader").load("header.html"); 
-});
-$(function(){
-    $("#adicionaFooter").load("footer.html"); 
-});
